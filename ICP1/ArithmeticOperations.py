5466afba69b9c686cad30ac2a1183a51df44a911
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2021-01-24 22:30:01 -0600

    Arthimatic

---
+++ ICP1/ArithmeticOperations.py
@@ -0,0 +1,22 @@
+# User input
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+# Addition Operation
+print("Addition :", a + b)
+
+# Subtraction Operation
+print("Subtraction :", a - b)
+
+# Multiplication Operation
+print("Multiplication :", a * b)
+
+# Modulus Operation
+print("Modulus :", a % b)
+
+# Division Operation
+print("Division :", a / b)
+
+# Exponential calculation
+print("Exponential calculation:", a ** b)
+
