f06347067d3d3cd8b1e7f5280c66e96a31088e71
vennelakovuru <60244042+vennelakovuru@users.noreply.github.com>
2021-02-12 23:20:00 -0600

    ICP4

--- ICP4/Correlation.py
+++ ICP4/Correlation.py
@@ -1,4 +1,4 @@
 import pandas as pd
 train = pd.read_csv('train_preprocessed.csv')
-# Finding the correlation
+# Finding the correlation using .corr function
 print("correlation between ‘survived’ column and ‘sex’ column :", train['Survived'].corr(train['Sex']))
