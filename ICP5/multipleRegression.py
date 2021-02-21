ffcc9027e324bd468025adec4870b2f38a0d3551
Vennela <vkrd9@umsystem.edu>
2021-02-20 20:09:15 -0600

    ICP5

--- ICP5/multipleRegression.py
+++ ICP5/multipleRegression.py
@@ -1,29 +1,19 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
+import pandas
 from sklearn import linear_model
-from sklearn.metrics import mean_squared_error
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
 
-data = pd.read_csv('data.csv')
-data = data.select_dtypes(include=[np.number]).interpolate().dropna()
+# reading restaurant data
+data = pandas.read_csv("data.csv")
 
-y = np.log(data.revenue)
-X = data.drop(['revenue', 'Id'], axis=1)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, random_state=42, test_size=.33)
+# read test and train data
+train = data.drop(['revenue', 'City Group', 'Type'], axis=1)
+test = data["revenue"].astype(str)
 
-lr = linear_model.LinearRegression()
-model = lr.fit(X_train, y_train)
+# Building a linear model and training the model with the train sets using the fit method
+reg = linear_model.LinearRegression()
+reg.fit(train, test)
+pred = reg.predict(train)
 
 # Finding R^2 and RMSE
-print("R^2: \n", model.score(X_test, y_test))
-predictions = model.predict(X_test)
-
-print('RMSE is: \n', mean_squared_error(y_test, predictions))
-
-
-sns.regplot(x=y_test, y=predictions, ci=None, color="b")
-# show the plot
-plt.show()
+print("R^2: %.2f" % r2_score(test, pred))
+print("RMSE: %.2f" % mean_squared_error(test, pred))
